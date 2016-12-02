d16b1d7517c77b9fa4fa6b064c9211530d60046c
Jason Marcotrigiano <BPCM87@gmail.com>
2016-12-02 00:28:24 -0500

    added mouse upgrades

--- CookiesRWe.js
+++ CookiesRWe.js
@@ -79,6 +79,10 @@ function upgradeUpgrades() {
             else if (i == "Titanium mouse") upgradeUpgrade(i);
             else if (i == "Unobtainium mouse") upgradeUpgrade(i);
             else if (i == "Wishalloy Mouse") upgradeUpgrade(i);
+            else if (i == "Lucky day") upgradeUpgrades(i);
+            else if (i == "Serendipity") upgradeUpgrades(i);
+            else if (i == "Get lucky") upgradeUpgrades(i);
+
         }
         if (CM.Cache.Upgrades[i].color == CM.Disp.colorBlue && i != "Golden switch [off]") {
             updateUpgradeAvailable = true;
