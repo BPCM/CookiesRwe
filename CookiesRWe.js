8cbc87a59d56ea10a8ba5f50416e960fbdff15bc
Jason Marcotrigiano <BPCM87@gmail.com>
2016-12-02 09:54:17 -0500

    added some basic logging for upgrades

--- CookiesRWe.js
+++ CookiesRWe.js
@@ -37,79 +37,80 @@ function upgradeBuildings() {
 function upgradeUpgrades() {
     updateUpgradeAvailable = false;
     for (var i in CM.Cache.Upgrades) {
-        if (CM.Cache.Upgrades[i].color == CM.Disp.colorGray && i != "Golden switch [off]") {
-            if (i == 'A festive hat')upgradeUpgrade(i);
-            else if (i == 'A crumbly egg')upgradeUpgrade(i);
-            //todo else if (i == "Elder Pledge")upgradeUpgrade(i);
-            else if (i == "Reinforced index finger") upgradeUpgrade(i);
-            else if (i == "Carpal tunnel prevention cream") upgradeUpgrade(i);
-            else if (i == "Ambidextrous") upgradeUpgrade(i);
-            else if (i == "Thousand fingers") upgradeUpgrade(i);
-            else if (i == "Million fingers") upgradeUpgrade(i);
-            else if (i == "Billion fingers") upgradeUpgrade(i);
-            else if (i == "Trillion fingers") upgradeUpgrade(i);
-            else if (i == "Quadrillion fingers") upgradeUpgrade(i);
-            else if (i == "Quintillion fingers") upgradeUpgrade(i);
-            else if (i == "Sextillion fingers") upgradeUpgrade(i);
-            else if (i == "Septillion fingers") upgradeUpgrade(i);
-            else if (i == "Octillion fingers") upgradeUpgrade(i);
-            else if (i == "Increased merriness") upgradeUpgrade(i);
-            else if (i == "Improved jolliness") upgradeUpgrade(i);
-            else if (i == "A lump of coal") upgradeUpgrade(i);
-            else if (i == "An itchy sweater") upgradeUpgrade(i);
-            else if (i == "Reindeer baking grounds") upgradeUpgrade(i);
-            else if (i == "Weighted sleighs") upgradeUpgrade(i);
-            else if (i == "Ho ho ho-flavored frosting") upgradeUpgrade(i);
-            else if (i == "Season savings") upgradeUpgrade(i);
-            else if (i == "Toy workshop") upgradeUpgrade(i);
-            else if (i == "Naughty list") upgradeUpgrade(i);
-            else if (i == "Santa's bottomless bag") upgradeUpgrade(i);
-            else if (i == "Santa's helpers") upgradeUpgrade(i);
-            else if (i == "Santa's legacy") upgradeUpgrade(i);
-            else if (i == "Santa's milk and cookies") upgradeUpgrade(i);
-            else if (i == "Santa's dominion") upgradeUpgrade(i);
-            else if (i == "Adamantium mouse") upgradeUpgrade(i);
-            else if (i == "Eludium mouse") upgradeUpgrade(i);
-            else if (i == "Fantasteel mouse") upgradeUpgrade(i);
-            else if (i == "Iron mouse") upgradeUpgrade(i);
-            else if (i == "Nevercrack mouse") upgradeUpgrade(i);
-            else if (i == "Plastic mouse") upgradeUpgrade(i);
-            else if (i == "Titanium mouse") upgradeUpgrade(i);
-            else if (i == "Unobtainium mouse") upgradeUpgrade(i);
-            else if (i == "Wishalloy mouse") upgradeUpgrade(i);
-            else if (i == "Lucky day") upgradeUpgrade(i);
-            else if (i == "Serendipity") upgradeUpgrade(i);
-            else if (i == "Get lucky") upgradeUpgrade(i);
-            else if (i == "Omelette") upgradeUpgrade(i);
-            else if (i == "Chicken egg") upgradeUpgrade(i);
-            else if (i == "Duck egg") upgradeUpgrade(i);
-            else if (i == "Turkey egg") upgradeUpgrade(i);
-            else if (i == "Quail egg") upgradeUpgrade(i);
-            else if (i == "Robin egg") upgradeUpgrade(i);
-            else if (i == "Ostrich egg") upgradeUpgrade(i);
-            else if (i == "Cassowary egg") upgradeUpgrade(i);
-            else if (i == "Salmon roe") upgradeUpgrade(i);
-            else if (i == "Frogspawn") upgradeUpgrade(i);
-            else if (i == "Shark egg") upgradeUpgrade(i);
-            else if (i == "Turtle egg") upgradeUpgrade(i);
-            else if (i == "Ant larva") upgradeUpgrade(i);
-            else if (i == "Golden goose egg") upgradeUpgrade(i);
-            else if (i == "Faberge egg") upgradeUpgrade(i);
-            else if (i == "Wrinklerspawn") upgradeUpgrade(i);
-            else if (i == "Cookie egg") upgradeUpgrade(i);
-            else if (i == "Omelette") upgradeUpgrade(i);
-            else if (i == "Chocolate egg") upgradeUpgrade(i);
-            else if (i == "Century egg") upgradeUpgrade(i);
-            else if (i == "egg") upgradeUpgrade(i);
+        var color = CM.Cache.Upgrades[i].color;
+        if (color == CM.Disp.colorGray && i != "Golden switch [off]") {
+            if (i == 'A festive hat')upgradeUpgrade(i, color);
+            else if (i == 'A crumbly egg')upgradeUpgrade(i, color);
+            //todo else if (i == "Elder Pledge")upgradeUpgrade(i,color);
+            else if (i == "Reinforced index finger") upgradeUpgrade(i, color);
+            else if (i == "Carpal tunnel prevention cream") upgradeUpgrade(i, color);
+            else if (i == "Ambidextrous") upgradeUpgrade(i, color);
+            else if (i == "Thousand fingers") upgradeUpgrade(i, color);
+            else if (i == "Million fingers") upgradeUpgrade(i, color);
+            else if (i == "Billion fingers") upgradeUpgrade(i, color);
+            else if (i == "Trillion fingers") upgradeUpgrade(i, color);
+            else if (i == "Quadrillion fingers") upgradeUpgrade(i, color);
+            else if (i == "Quintillion fingers") upgradeUpgrade(i, color);
+            else if (i == "Sextillion fingers") upgradeUpgrade(i, color);
+            else if (i == "Septillion fingers") upgradeUpgrade(i, color);
+            else if (i == "Octillion fingers") upgradeUpgrade(i, color);
+            else if (i == "Increased merriness") upgradeUpgrade(i, color);
+            else if (i == "Improved jolliness") upgradeUpgrade(i, color);
+            else if (i == "A lump of coal") upgradeUpgrade(i, color);
+            else if (i == "An itchy sweater") upgradeUpgrade(i, color);
+            else if (i == "Reindeer baking grounds") upgradeUpgrade(i, color);
+            else if (i == "Weighted sleighs") upgradeUpgrade(i, color);
+            else if (i == "Ho ho ho-flavored frosting") upgradeUpgrade(i, color);
+            else if (i == "Season savings") upgradeUpgrade(i, color);
+            else if (i == "Toy workshop") upgradeUpgrade(i, color);
+            else if (i == "Naughty list") upgradeUpgrade(i, color);
+            else if (i == "Santa's bottomless bag") upgradeUpgrade(i, color);
+            else if (i == "Santa's helpers") upgradeUpgrade(i, color);
+            else if (i == "Santa's legacy") upgradeUpgrade(i, color);
+            else if (i == "Santa's milk and cookies") upgradeUpgrade(i, color);
+            else if (i == "Santa's dominion") upgradeUpgrade(i, color);
+            else if (i == "Adamantium mouse") upgradeUpgrade(i, color);
+            else if (i == "Eludium mouse") upgradeUpgrade(i, color);
+            else if (i == "Fantasteel mouse") upgradeUpgrade(i, color);
+            else if (i == "Iron mouse") upgradeUpgrade(i, color);
+            else if (i == "Nevercrack mouse") upgradeUpgrade(i, color);
+            else if (i == "Plastic mouse") upgradeUpgrade(i, color);
+            else if (i == "Titanium mouse") upgradeUpgrade(i, color);
+            else if (i == "Unobtainium mouse") upgradeUpgrade(i, color);
+            else if (i == "Wishalloy mouse") upgradeUpgrade(i, color);
+            else if (i == "Lucky day") upgradeUpgrade(i, color);
+            else if (i == "Serendipity") upgradeUpgrade(i, color);
+            else if (i == "Get lucky") upgradeUpgrade(i, color);
+            else if (i == "Omelette") upgradeUpgrade(i, color);
+            else if (i == "Chicken egg") upgradeUpgrade(i, color);
+            else if (i == "Duck egg") upgradeUpgrade(i, color);
+            else if (i == "Turkey egg") upgradeUpgrade(i, color);
+            else if (i == "Quail egg") upgradeUpgrade(i, color);
+            else if (i == "Robin egg") upgradeUpgrade(i, color);
+            else if (i == "Ostrich egg") upgradeUpgrade(i, color);
+            else if (i == "Cassowary egg") upgradeUpgrade(i, color);
+            else if (i == "Salmon roe") upgradeUpgrade(i, color);
+            else if (i == "Frogspawn") upgradeUpgrade(i, color);
+            else if (i == "Shark egg") upgradeUpgrade(i, color);
+            else if (i == "Turtle egg") upgradeUpgrade(i, color);
+            else if (i == "Ant larva") upgradeUpgrade(i, color);
+            else if (i == "Golden goose egg") upgradeUpgrade(i, color);
+            else if (i == "Faberge egg") upgradeUpgrade(i, color);
+            else if (i == "Wrinklerspawn") upgradeUpgrade(i, color);
+            else if (i == "Cookie egg") upgradeUpgrade(i, color);
+            else if (i == "Omelette") upgradeUpgrade(i, color);
+            else if (i == "Chocolate egg") upgradeUpgrade(i, color);
+            else if (i == "Century egg") upgradeUpgrade(i, color);
+            else if (i == "egg") upgradeUpgrade(i, color);
 
         }
-        if (CM.Cache.Upgrades[i].color == CM.Disp.colorBlue && i != "Golden switch [off]") {
+        if (color == CM.Disp.colorBlue && i != "Golden switch [off]") {
             updateUpgradeAvailable = true;
-            upgradeUpgrade(i);
+            upgradeUpgrade(i, color);
             break;
-        } else if (CM.Cache.Upgrades[i].color == CM.Disp.colorGreen && i != "Golden switch [off]") {
+        } else if (color == CM.Disp.colorGreen && i != "Golden switch [off]") {
             updateUpgradeAvailable = true;
-            upgradeUpgrade(i);
+            upgradeUpgrade(i, color);
             break;
         }
     }
@@ -140,8 +141,9 @@ function upgradeObject(object) {
 
 }
 
-function upgradeUpgrade(upgrade) {
+function upgradeUpgrade(upgrade, color) {
     if (Game.Upgrades[upgrade].bought == 0 && Game.cookiesd >= Game.Upgrades[upgrade].basePrice) {
+        console.log(upgrade + ": " + color);
         Game.Upgrades[upgrade].buy(1);
         clearInterval(upgradeInterval);
         if (debug) console.log(fInterval);
