a52b8f03ec63a48d93060f8e5a68a1fc2177bf55
Jason Marcotrigiano <BPCM87@gmail.com>
2016-12-03 21:05:25 -0500

    adding a checker for the seasonal cookies

--- CookiesRWe.js
+++ CookiesRWe.js
@@ -1,14 +1,27 @@
 waitForGame(250);
-var clickLoop, upgradeInterval, dInterval = 1000, fInterval = 10, updateUpgradeAvailable = false, debug = false, clickLoopEnabled = true, elderPledgeUpgrade = true;
+var debug = false, dInterval = 1000, fInterval = 10,
+    clickLoop, upgradeInterval,
+    updateUpgradeAvailable = false, clickLoopEnabled = true, elderPledgeUpgrade = true;
 //todo basePrice is not actual cost!
 //todo show which seasonal items are missing
-//todo disable click
 //todo easter & halloween cookie find/refresh
-//todo add eggs to auto-buy
 function Init() {
-//sad
-    //PlaySound('snd/click'+(Game.cookieClickSound)+'.mp3',0.5);
-    Game.playCookieClickSound=function(){};
+
+    Game.playCookieClickSound = function () { //Removes clicking sound from game, sorry dude
+    };
+
+    if (typeof CM.Cache.Upgrades["Bunny biscuit"] != 'undefined') {
+        if (!haveAllEasterEggs()) {
+            Game.Popup("You are missing Easter eggs!");
+        }
+        if (!haveAllHalloweenCookies()) {
+            Game.Popup("You are missing Halloween cookies");
+        }
+        if (!haveAllValentineCookies) {
+            Game.Popup("You are missing Valentine's day cookies");
+        }
+    }
+
     upgradeInterval = setInterval(upgradeBuildings, dInterval);
     clickLoop = setInterval(clickBigCookie, 16);
     setInterval(function () {
@@ -17,13 +30,11 @@ function Init() {
                 Game.shimmers[g].pop();
             }
         }
-
         if (Game.hasBuff('Clot') > 0) {
             Game.WriteSave();
             location.reload();
         }
     }, 1000);
-
 
     AddEvent(window, 'keydown', function (e) {
         if (!Game.OnAscend && Game.AscendTimer == 0) {
@@ -39,24 +50,69 @@ function Init() {
                 }
             }
             else if (/*e.ctrlKey &&*/ e.keyCode == 104) { //NUM8
-                if (elderPledgeUpgrade) {
-                    elderPledgeUpgrade = false;
-                    Game.Popup('Disabled elder pledge auto-buy');
-                } else {
-                    elderPledgeUpgrade = true;
-                    Game.Popup('Enabled elder pledge auto-buy');
-
-                }
-            }//ctrl-o opens the import menu
+                disableElderPledge();
+            }
         }
         Game.keys[e.keyCode] = 1;
     });
 
-    //  window.onbeforeunload = confirmWinClose;
+//  window.onbeforeunload = confirmWinClose;
 }
+
+function haveAllValentineCookies() {
+    var caughtThemAll = true;
+    if (Game.Upgrades["Pure heart biscuits"].bought = 0)        caughtThemAll = false;
+    else if (Game.Upgrades["Ardent heart biscuits"].bought = 0)        caughtThemAll = false;
+    else if (Game.Upgrades["Sour heart biscuits"].bought = 0)        caughtThemAll = false;
+    else if (Game.Upgrades["Weeping heart biscuits"].bought = 0)        caughtThemAll = false;
+    else if (Game.Upgrades["Golden heart biscuits"].bought = 0)        caughtThemAll = false;
+    else if (Game.Upgrades["Eternal heart biscuits"].bought = 0)        caughtThemAll = false;
+    return caughtThemAll;
+}
+
+function haveAllEasterEggs() {
+    var caughtThemAll = true;
+    if (Game.Upgrades["Omelette"].bought = 0)        caughtThemAll = false;
+    else if (Game.Upgrades["Chicken egg"].bought = 0)        caughtThemAll = false;
+    else if (Game.Upgrades["Duck egg"].bought = 0)        caughtThemAll = false;
+    else if (Game.Upgrades["Turkey egg"].bought = 0)        caughtThemAll = false;
+    else if (Game.Upgrades["Quail egg"].bought = 0)        caughtThemAll = false;
+    else if (Game.Upgrades["Robin egg"].bought = 0)        caughtThemAll = false;
+    else if (Game.Upgrades["Ostrich egg"].bought = 0)        caughtThemAll = false;
+    else if (Game.Upgrades["Cassowary egg"].bought = 0)        caughtThemAll = false;
+    else if (Game.Upgrades["Salmon roe"].bought = 0)        caughtThemAll = false;
+    else if (Game.Upgrades["Frogspawn"].bought = 0)        caughtThemAll = false;
+    else if (Game.Upgrades["Shark egg"].bought = 0)        caughtThemAll = false;
+    else if (Game.Upgrades["Turtle egg"].bought = 0)        caughtThemAll = false;
+    else if (Game.Upgrades["Ant larva"].bought = 0)        caughtThemAll = false;
+    else if (Game.Upgrades["Golden goose egg"].bought = 0)        caughtThemAll = false;
+    else if (Game.Upgrades["Faberge egg"].bought = 0)        caughtThemAll = false;
+    else if (Game.Upgrades["Wrinklerspawn"].bought = 0)        caughtThemAll = false;
+    else if (Game.Upgrades["Cookie egg"].bought = 0)        caughtThemAll = false;
+    else if (Game.Upgrades["Omelette"].bought = 0)        caughtThemAll = false;
+    else if (Game.Upgrades["Chocolate egg"].bought = 0)        caughtThemAll = false;
+    else if (Game.Upgrades["Century egg"].bought = 0)        caughtThemAll = false;
+    else if (Game.Upgrades["egg"].bought = 0)        caughtThemAll = false;
+    return caughtThemAll;
+}
+
+function haveAllHalloweenCookies() {
+    var caughtThemAll = true;
+    if (Game.Upgrades["Skull cookies"].bought = 0)        caughtThemAll = false;
+    else if (Game.Upgrades["Ghost cookies"].bought = 0)        caughtThemAll = false;
+    else if (Game.Upgrades["Bat cookies"].bought = 0)        caughtThemAll = false;
+    else if (Game.Upgrades["Slime cookies"].bought = 0)        caughtThemAll = false;
+    else if (Game.Upgrades["Pumpkin cookies"].bought = 0)        caughtThemAll = false;
+    else if (Game.Upgrades["Eyeball cookies"].bought = 0)        caughtThemAll = false;
+    else if (Game.Upgrades["Spider cookies"].bought = 0)        caughtThemAll = false;
+    return caughtThemAll
+}
+
+
 function clickBigCookie() {
     Game.ClickCookie();
 }
+
 
 function upgradeBuildings() {
     if (debug) console.log("upgradeBuildings: " + new Date());
@@ -68,12 +124,40 @@ function upgradeBuildings() {
 
 function upgradeUpgrades() {
     updateUpgradeAvailable = false;
+    /*    if (Game.season == 'easter') {
+     getEasterEggs();
+     }*/
     for (var i in CM.Cache.Upgrades) {
         var color = CM.Cache.Upgrades[i].color;
         if (color == CM.Disp.colorGray && i != "Golden switch [off]") {
             if (i == 'A festive hat')upgradeUpgrade(i, color);
             else if (i == 'A crumbly egg')upgradeUpgrade(i, color);
             else if (elderPledgeUpgrade && i == "Elder Pledge")upgradeUpgrade(i, color);
+
+
+            else if (i == "Omelette") upgradeUpgrade(i, color);
+            else if (i == "Chicken egg") upgradeUpgrade(i, color);
+            else if (i == "Duck egg") upgradeUpgrade(i, color);
+            else if (i == "Turkey egg") upgradeUpgrade(i, color);
+            else if (i == "Quail egg") upgradeUpgrade(i, color);
+            else if (i == "Robin egg") upgradeUpgrade(i, color);
+            else if (i == "Ostrich egg") upgradeUpgrade(i, color);
+            else if (i == "Cassowary egg") upgradeUpgrade(i, color);
+            else if (i == "Salmon roe") upgradeUpgrade(i, color);
+            else if (i == "Frogspawn") upgradeUpgrade(i, color);
+            else if (i == "Shark egg") upgradeUpgrade(i, color);
+            else if (i == "Turtle egg") upgradeUpgrade(i, color);
+            else if (i == "Ant larva") upgradeUpgrade(i, color);
+            else if (i == "Golden goose egg") upgradeUpgrade(i, color);
+            else if (i == "Faberge egg") upgradeUpgrade(i, color);
+            else if (i == "Wrinklerspawn") upgradeUpgrade(i, color);
+            else if (i == "Cookie egg") upgradeUpgrade(i, color);
+            else if (i == "Omelette") upgradeUpgrade(i, color);
+            //else if (i == "Chocolate egg") upgradeUpgrade(i, color); I think you are supposed to save this egg for when you ascend
+            else if (i == "Century egg") upgradeUpgrade(i, color);
+            else if (i == "egg") upgradeUpgrade(i, color);
+
+
             else if (i == "Reinforced index finger") upgradeUpgrade(i, color);
             else if (i == "Carpal tunnel prevention cream") upgradeUpgrade(i, color);
             else if (i == "Ambidextrous") upgradeUpgrade(i, color);
@@ -96,6 +180,7 @@ function upgradeUpgrades() {
             else if (i == "Season savings") upgradeUpgrade(i, color);
             else if (i == "Toy workshop") upgradeUpgrade(i, color);
             else if (i == "Naughty list") upgradeUpgrade(i, color);
+            else if (i == "Sacrificial rolling pills") upgradeUpgrade(i, color);
             else if (i == "Santa's bottomless bag") upgradeUpgrade(i, color);
             else if (i == "Santa's helpers") upgradeUpgrade(i, color);
             else if (i == "Santa's legacy") upgradeUpgrade(i, color);
@@ -113,27 +198,7 @@ function upgradeUpgrades() {
             else if (i == "Lucky day") upgradeUpgrade(i, color);
             else if (i == "Serendipity") upgradeUpgrade(i, color);
             else if (i == "Get lucky") upgradeUpgrade(i, color);
-            else if (i == "Omelette") upgradeUpgrade(i, color);
-            else if (i == "Chicken egg") upgradeUpgrade(i, color);
-            else if (i == "Duck egg") upgradeUpgrade(i, color);
-            else if (i == "Turkey egg") upgradeUpgrade(i, color);
-            else if (i == "Quail egg") upgradeUpgrade(i, color);
-            else if (i == "Robin egg") upgradeUpgrade(i, color);
-            else if (i == "Ostrich egg") upgradeUpgrade(i, color);
-            else if (i == "Cassowary egg") upgradeUpgrade(i, color);
-            else if (i == "Salmon roe") upgradeUpgrade(i, color);
-            else if (i == "Frogspawn") upgradeUpgrade(i, color);
-            else if (i == "Shark egg") upgradeUpgrade(i, color);
-            else if (i == "Turtle egg") upgradeUpgrade(i, color);
-            else if (i == "Ant larva") upgradeUpgrade(i, color);
-            else if (i == "Golden goose egg") upgradeUpgrade(i, color);
-            else if (i == "Faberge egg") upgradeUpgrade(i, color);
-            else if (i == "Wrinklerspawn") upgradeUpgrade(i, color);
-            else if (i == "Cookie egg") upgradeUpgrade(i, color);
-            else if (i == "Omelette") upgradeUpgrade(i, color);
-            //else if (i == "Chocolate egg") upgradeUpgrade(i, color); I think you are supposed to save this egg for when you ascend
-            else if (i == "Century egg") upgradeUpgrade(i, color);
-            else if (i == "egg") upgradeUpgrade(i, color);
+
             else if (i == "Skull cookies") upgradeUpgrade(i, color);
             else if (i == "Ghost cookies") upgradeUpgrade(i, color);
             else if (i == "Bat cookies") upgradeUpgrade(i, color);
@@ -153,6 +218,7 @@ function upgradeUpgrades() {
         }
     }
 }
+
 
 function upgradeObjects() {
     if (!updateUpgradeAvailable)
@@ -201,5 +267,15 @@ function waitForGame(delay) {
         setTimeout(function () {
             waitForGame(delay);
         }, delay);
+    }
+}
+
+function disableElderPledge() {
+    if (elderPledgeUpgrade) {
+        elderPledgeUpgrade = false;
+        Game.Popup('Disabled elder pledge auto-buy');
+    } else {
+        elderPledgeUpgrade = true;
+        Game.Popup('Enabled elder pledge auto-buy');
     }
 }
